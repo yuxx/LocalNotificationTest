7944b22fd56c8d9b7ec86741a5e0c34af51eb60b
yu++ <7052856+yuxx@users.noreply.github.com>
2023-01-06 17:58:32 +0900

    Initial Commit

---
+++ BackgroundTaskTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BackgroundTaskTest
+//
+//  Created by yuji sakata on 2023/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
